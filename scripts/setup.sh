9fbaffafedf24c347043f211c8c502b05743a2e9
Chris McKeever <cgmckeever@rebelliondefense.com>
2023-08-07 21:48:27 -0500

    Wireguard quick install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,7 @@ source ${SCRIPT_PATH}/color.inc.sh
 
 NIC=$(route | grep default | awk '{print $8}')
 
+echo; echo
 read -p "What is the Wireguard Network? default [10.10.0.0/24] " NETWORK
 NETWORK=${NETWORK:-"10.10.0.0/24"}
 read A B C D <<<"${NETWORK//./ }"
@@ -22,6 +23,7 @@ sudo touch /etc/wireguard/privatekey
 sudo touch /etc/wireguard/publickey
 
 if [[ "$(cat /etc/wireguard/privatekey)" != "" && "$(cat /etc/wireguard/publickey)" != "" ]]; then
+    printf $info "\n\nFound existing keys \n"
     read -p "Generate new Wireguard keys [Y/n]? " GENKEYS
     GENKEYS=${GENKEYS:-"y"}
 fi
@@ -41,6 +43,16 @@ sudo sed -i \
     -e "s#__PRIVATE_KEY#${PRIVATE_KEY}#g" \
     -e "s/__NIC/${NIC}/g" \
     /etc/wireguard/wg0.conf
+
+sudo cp ${TEMPLATE_PATH}/wireguard.profile.sh.tmpl /etc/profile.d/wireguard.profile.sh
+
+sudo sed -i \
+    -e "s/__WG_NETWORK/${NETWORK}/g" \
+    -e "s/__WG_PUBLIC_KEY/${PUBLIC_KEY}/g" \
+    -e "s#__WG_PORT#${PORT}#g" \
+    /etc/profile.d/wireguard.profile.sh
+
+sudo chmod 755 /etc/profile.d/wireguard.profile.sh
 
 #sudo systemctl start wg-quick@wg0
 #sudo systemctl enable wg-quick@wg0
