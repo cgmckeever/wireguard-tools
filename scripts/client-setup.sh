7783856ae8f4abeb7295c3cd16343f6393727dc3
cgmckeever <cgmckeever@ironhide.local>
2024-11-30 12:28:00 -0600

    use existing client configs

--- scripts/client-setup.sh
+++ scripts/client-setup.sh
@@ -24,33 +24,31 @@ if [[ -e "${CLIENT_CONF}" ]];then
 
     WG_ALLOWED_IPS_DEFAULT=$(sed -n 's/AllowedIPs = \(.*\)/\1/p' "$file_path")
 fi 
-    printf $info "\nWireguard Network is ${WG_NETWORK}"
-    IFS='.' read A B C D <<< ${WG_NETWORK}
 
-    prompt "IP to allocate client: ${A}.${B}.${C}.${LAST_OCTET_PROMPT}" LAST_OCTET
-    LAST_OCTET=${LAST_OCTET:-${LAST_OCTET_DEFAULT}}
-    IP=${A}.${B}.${C}.${LAST_OCTET}
+printf $info "\nWireguard Network is ${WG_NETWORK}"
+IFS='.' read A B C D <<< ${WG_NETWORK}
 
-    WG_ALLOWED_IPS_DEFAULT=${WG_ALLOWED_IPS_DEFAULT:-${WG_DEFAULT_ALLOWED_IPS}}
-    prompt "Allowed IPs - default [${WG_ALLOWED_IPS_DEFAULT}]:" WG_ALLOWED_IPS
-    WG_ALLOWED_IPS=${WG_ALLOWED_IPS:-${WG_ALLOWED_IPS_DEFAULT}}
+prompt "IP to allocate client: ${A}.${B}.${C}.${LAST_OCTET_PROMPT}" LAST_OCTET
+LAST_OCTET=${LAST_OCTET:-${LAST_OCTET_DEFAULT}}
+IP=${A}.${B}.${C}.${LAST_OCTET}
 
-    PRIVATE_KEY_PROMPT=${PRIVATE_KEY_DEFAULT:-"enter to create new key-pair"}
-    PRIVATE_KEY_DEFAULT=${PRIVATE_KEY_DEFAULT:-$(wg genkey)}
-    prompt "Enter an existing Wireguard Private Key [${PRIVATE_KEY_DEFAULT}]:" PRIVATE_KEY
-    PRIVATE_KEY=${PRIVATE_KEY:-${PRIVATE_KEY_DEFAULT}}
-    PUBLIC_KEY=$(echo ${PRIVATE_KEY} | wg pubkey)
+WG_ALLOWED_IPS_DEFAULT=${WG_ALLOWED_IPS_DEFAULT:-${WG_DEFAULT_ALLOWED_IPS}}
+prompt "Allowed IPs - default [${WG_ALLOWED_IPS_DEFAULT}]:" WG_ALLOWED_IPS
+WG_ALLOWED_IPS=${WG_ALLOWED_IPS:-${WG_ALLOWED_IPS_DEFAULT}}
 
-    sudo sed \
-        -e "s#__WG_SERVER_IP#${SERVER_IP}#g" \
-        -e "s/__WG_PORT/${WG_PORT}/g" \
-        -e "s#__WG_SERVER_PUBLIC_KEY#${WG_SERVER_PUBLIC_KEY}#g" \
-        -e "s#__WG_CLIENT_PRIVATE_KEY#${PRIVATE_KEY}#g" \
-        -e "s#__WG_IP#${IP}/32#g" \
-        -e "s#__WG_ALLOWED_IPS#${WG_ALLOWED_IPS}#g" \
-        ${TEMPLATE_PATH}/client.conf.tmpl > ${CLIENT_CONF}
-else
-    sed -i "/^\[Peer\]/,/\[/s/^PublicKey = .*/PublicKey = ${WG_SERVER_PUBLIC_KEY}/" "${CLIENT_CONF}"
-fi 
+PRIVATE_KEY_PROMPT=${PRIVATE_KEY_DEFAULT:-"enter to create new key-pair"}
+PRIVATE_KEY_DEFAULT=${PRIVATE_KEY_DEFAULT:-$(wg genkey)}
+prompt "Enter an existing Wireguard Private Key [${PRIVATE_KEY_DEFAULT}]:" PRIVATE_KEY
+PRIVATE_KEY=${PRIVATE_KEY:-${PRIVATE_KEY_DEFAULT}}
+PUBLIC_KEY=$(echo ${PRIVATE_KEY} | wg pubkey)
+
+sudo sed \
+    -e "s#__WG_SERVER_IP#${SERVER_IP}#g" \
+    -e "s/__WG_PORT/${WG_PORT}/g" \
+    -e "s#__WG_SERVER_PUBLIC_KEY#${WG_SERVER_PUBLIC_KEY}#g" \
+    -e "s#__WG_CLIENT_PRIVATE_KEY#${PRIVATE_KEY}#g" \
+    -e "s#__WG_IP#${IP}/32#g" \
+    -e "s#__WG_ALLOWED_IPS#${WG_ALLOWED_IPS}#g" \
+    ${TEMPLATE_PATH}/client.conf.tmpl > ${CLIENT_CONF}
 
 ${SCRIPT_PATH}/client-conf.sh ${WG_ALIAS}
