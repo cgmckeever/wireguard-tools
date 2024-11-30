8819e79b52a2b61ef397f6ec95e4f185635b361b
cgmckeever <cgmckeever@ironhide.local>
2024-11-30 13:36:58 -0600

    remove script

--- scripts/client-remove.sh
+++ scripts/client-remove.sh
@@ -9,12 +9,26 @@ source ${CONFIG_PATH}
 
 WG=$(wg)
 
+ALIASES=()
 PEERS=()
 IPS=()
 
 while read -r LINE; do
     if [[ "${LINE}" =~ ^peer:\ (.*) ]];then
-        PEERS+=("${BASH_REMATCH[1]}")
+    	PEER_KEY=${BASH_REMATCH[1]}
+        PEERS+=(${PEER_KEY})
+
+        for CLIENT in "${CLIENT_PATH}"/*.conf; do
+		    ALIAS=$(basename "${CLIENT}" .conf)
+
+		    PRIVATE_KEY=$(grep -oP 'PrivateKey = \K.+' "${CLIENT}")
+		    PUBLIC_KEY=$(echo "${PRIVATE_KEY}" | wg pubkey)
+
+		    if [[ "${PUBLIC_KEY}" == "${PEER_KEY}" ]]; then
+		        ALIASES+=(${ALIAS})
+		    fi
+		done
+
     elif [[ "${LINE}" =~ ^[[:space:]]*allowed\ ips:\ (.*) ]];then
         IPS+=("${BASH_REMATCH[1]}")
     fi
@@ -22,7 +36,7 @@ done <<< "${WG}"
 
 printf $info "\nPeer List:\n"
 for i in "${!PEERS[@]}"; do
-    printf $warn "$((i + 1)). ${PEERS[$i]} / ${IPS[$i]}\n"
+    printf $warn "$((i + 1)). ${ALIASES[$i]}: ${PEERS[$i]} / ${IPS[$i]}\n"
 done
 
 prompt "Which client do you want to remove?" PEER
