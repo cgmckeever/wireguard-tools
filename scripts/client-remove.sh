365e0852c467ef0c0ef0a0eab0992b4565bad2f7
cgmckeever <cgmckeever@ironhide.local>
2024-11-30 13:20:23 -0600

    remove script

--- scripts/client-remove.sh
+++ scripts/client-remove.sh
@@ -6,11 +6,34 @@ source ${CONFIG_PATH}
 
 # ==============================
 
-echo
-sudo wg
-echo
-prompt "Which client do you want to remove?" PUBLIC_KEY
 
-sudo wg set wg0 peer ${PUBLIC_KEY} remove
-sudo wg
-echo
+WG=$(wg)
+
+PEERS=()
+IPS=()
+
+while read -r LINE; do
+    if [[ "${LINE}" =~ ^peer:\ (.*) ]];then
+        PEERS+=("${BASH_REMATCH[1]}")
+    elif [[ "$line" =~ ^allowed\ ips:\ (.*) ]];then
+        IPS+=("${BASH_REMATCH[1]}")
+    fi
+done <<< "${WG}"
+
+printf $info "\nPeer List:"
+for i in "${!peers[@]}"; do
+    printf $warn "$((i + 1)). ${PEERS[$i]} / ${IPS[$i]}"
+done
+
+prompt "Which client do you want to remove?" PEER
+
+if [[ "${PEER}" -gt 0 && "${PEER}" -le "${#PEERS[@]}" ]];then
+    sudo wg set wg0 peer "${PEERS[$((PEER - 1))]}" remove
+
+    echo
+	sudo wg
+	echo
+else
+	printf $warn "\nInvalid selection. \n"
+fi
+
